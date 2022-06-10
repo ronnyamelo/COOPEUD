76fe49ba31717e0c06d16f9c088513d4ae649ce7
ronnyamelo <ron_e@hotmail.es>
2022-06-10 00:05:27 -0400

    changes made by python, adding these

---
+++ COOPEUD/base/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def inicio(request):
+    return render(request,'inicio.html') 
+
+def nosotros(request):
+    return render(request,'nosotros.html')
