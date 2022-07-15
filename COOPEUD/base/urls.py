d45e227f364ca67a14d9b8e91100b63d76786ad9
Junior Rivas <junior>
2022-07-14 23:52:14 -0400

    Remove project files

--- COOPEUD/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.inicio, name='inicio'),
-    path('nosotros/',views.nosotros,name='nostros'),
-    path('back/', views.adminViewSet, name='index')
-]
