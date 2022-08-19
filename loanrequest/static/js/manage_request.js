7fed07b5f7505f8d33e17e249afd4a9f04c3e734
Junior Rivas <junior>
2022-08-19 02:30:21 -0400

    Show options depending on request status

---
+++ loanrequest/static/js/manage_request.js
@@ -0,0 +1,14 @@
+const approveRequestBtn = document.getElementById('approve-request');
+const denyRequestBtn = document.getElementById('deny-request');
+const cancelRequestBtn = document.getElementById('cancel-request');
+const completeRequestBtn = document.getElementById('complete-request');
+
+
+
+async function sendRequest(url, payload, params) {
+    fetch(url, {
+        
+    })
+}
+
+
