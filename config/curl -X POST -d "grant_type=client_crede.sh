0b6e4c5d6af9350866de32d7bfcfcf4f5551affb
Junior Rivas <junior>
2022-09-04 13:54:02 -0400

    Remove unneeded file

--- config/curl -X POST -d "grant_type=client_crede.sh
+++
@@ -1,8 +0,0 @@
-curl -X POST -d "grant_type=client_credentials" -u"postman:postman_01" http://localhost:8000/o/token/
-
-OAUTH2_PROVIDER = {
-    # this is the list of available scopes
-    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
-}
-
-curl -X POST -d "grant_type=password&username=junior&password=Zorro.0709" -u"postman2:postman_02" http://localhost:8000/o/token/
--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -20,7 +5,6 @@ from loanrequest import views
 from coopeud import views as web_views
 
 router = routers.DefaultRouter()
-# router.register(r'api/solicitudes', views.LoanRequestViewSet)
 router.register(r'', views.LoanRequestViewSet)
 
 urlpatterns = [
@@ -35,12 +19,5 @@ urlpatterns = [
     path('logout/', views.logoutView, name='login'),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('nosotros/', web_views.history, name = 'history'),
-    # path("accounts/", include("django.contrib.auth.urls")),
-    # path("admin/", admin.site.urls),
-    # path('admin/', views.TestViewSet.as_view({'get': 'list'}), name='test'),
-    # path('test/', views.test, name='token')
-
     path('solicitudes/', web_views.handle_loan_request, name="handle_loan_request"),
-
-
 ]
