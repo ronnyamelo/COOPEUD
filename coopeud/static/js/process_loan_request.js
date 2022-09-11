66fd19bdc01835f7bfb66fcb8301eef20fcec6e4
Junior Rivas <junior>
2022-09-11 18:09:44 -0400

    Add error messages to loan request form

--- coopeud/static/js/process_loan_request.js
+++
@@ -1,7 +0,0 @@
-// const sendLoanRequestBtn = document.getElementById("sendLoanRequest");
-
-
-// sendLoanRequestBtn.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     alert("text: " + sendLoanRequestBtn.innerText);
-// })
